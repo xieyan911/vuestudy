ed59026af45c62a6b57783e84a1718fd3d5e181b
CN\xieyan <javatalent@163.com>
2020-04-14 15:53:53 +0800

    引入iview

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 引入iview
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+Vue.use(ViewUI);
+
 new Vue({
   router,
   store,
